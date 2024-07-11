31b909d06d74fd32e7cc834b9cc07715f74c297f
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-07-11 13:52:20 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-07.2@ad51ad0854c30f09ec8ad2252fbb45a809aea8c9 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{g as o}from"./assets/gallery-items-d77370d5.js";/* empty css                      */console.log(o);
+import{g as a}from"./assets/gallery-items-d77370d5.js";/* empty css                      */const r=document.querySelector(".gallery"),e=a.map(l=>`<li class="gallery__item"><a class="gallery__link"><img class="gallery__image" src=${l.preview} alt=${l.description}/></a></li>`).join("");r.innerHTML=e;console.log(e);
 //# sourceMappingURL=commonHelpers.js.map
