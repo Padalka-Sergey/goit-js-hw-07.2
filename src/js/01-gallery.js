ad51ad0854c30f09ec8ad2252fbb45a809aea8c9
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-07-11 16:52:00 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    galleryItem =>
+      `<li class="gallery__item"><a class="gallery__link"><img class="gallery__image" src=${galleryItem.preview} alt=${galleryItem.description}/></a></li>`
+  )
+  .join('');
+
+galleryRef.innerHTML = markup;
+console.log(markup);
