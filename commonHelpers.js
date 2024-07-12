14432eaf2d45a4b97003c4d6ecbf3a289e8a2c2a
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-07-12 18:30:18 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-07.2@89642b1bd98081ed055caaf58815777e7c14ca4c 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,4 @@
-import{g as a}from"./assets/gallery-items-d77370d5.js";/* empty css                      */const r=document.querySelector(".gallery"),e=a.map(l=>`<li class="gallery__item"><a class="gallery__link"><img class="gallery__image" src=${l.preview} alt=${l.description}/></a></li>`).join("");r.innerHTML=e;console.log(e);
+import{g as l}from"./assets/gallery-items-d77370d5.js";/* empty css                      */const r=document.querySelector(".gallery"),c=l.map(({preview:e,description:a,original:t})=>`<li class="gallery__item"><a class="gallery__link" href=${t}><img class="gallery__image" src=${e} data-source=${t} alt=${a}/></a></li>`).join("");r.innerHTML=c;r.addEventListener("click",s);function s(e){if(e.preventDefault(),e.target.nodeName!=="IMG")return;basicLightbox.create(`
+    <img src=${e.target.dataset.source} width="800" height="600">
+`).show(),console.dir(e.target)}
 //# sourceMappingURL=commonHelpers.js.map
