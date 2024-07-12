89642b1bd98081ed055caaf58815777e7c14ca4c
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-07-12 21:29:59 +0300

    add cdn

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,29 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 const galleryRef = document.querySelector('.gallery');
 const markup = galleryItems
   .map(
-    galleryItem =>
-      `<li class="gallery__item"><a class="gallery__link"><img class="gallery__image" src=${galleryItem.preview} alt=${galleryItem.description}/></a></li>`
+    ({ preview, description, original }) =>
+      `<li class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a></li>`
   )
   .join('');
 
 galleryRef.innerHTML = markup;
-console.log(markup);
+
+galleryRef.addEventListener('click', clickImageHandler);
+
+function clickImageHandler(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    // console.log(evt.target.nodeName);
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src=${evt.target.dataset.source} width="800" height="600">
+`);
+
+  instance.show();
+  console.dir(evt.target);
+}
