4cc03b97bc28a96a0293e89f08d24e08c4b497d1
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-07-14 12:57:48 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-07.2@f09725a39994f67127820510870156eb40065031 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{g as l}from"./assets/gallery-items-d77370d5.js";/* empty css                      */const r=document.querySelector(".gallery"),c=l.map(({preview:e,description:a,original:t})=>`<li class="gallery__item"><a class="gallery__link" href=${t}><img class="gallery__image" src=${e} data-source=${t} alt=${a}/></a></li>`).join("");r.innerHTML=c;r.addEventListener("click",s);function s(e){if(e.preventDefault(),e.target.nodeName!=="IMG")return;basicLightbox.create(`
+import{g as s}from"./assets/gallery-items-d77370d5.js";/* empty css                      */let t=null;const a=document.querySelector(".gallery"),i=o(s);a.innerHTML=i;a.addEventListener("click",m);function o(e){return e.map(({preview:l,description:c,original:r})=>`<li class="gallery__item"><a class="gallery__link" href=${r}><img class="gallery__image" src=${l} data-source=${r} alt=${c}/></a></li>`).join("")}function m(e){e.preventDefault(),e.target.nodeName==="IMG"&&(t=basicLightbox.create(`
     <img src=${e.target.dataset.source} width="800" height="600">
-`).show(),console.dir(e.target)}
+`),t.show(()=>document.addEventListener("keydown",n)))}function n(e){e.code==="Escape"&&t.close(()=>document.removeEventListener("keydown",n))}
 //# sourceMappingURL=commonHelpers.js.map
