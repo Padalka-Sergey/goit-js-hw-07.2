104da4ede74a18d355e45a7e2996c87bb54af0f8
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-07-15 16:49:11 +0300

    hw-07.2 simplelightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const markup = createGalleryItems(galleryItems);
+
+galleryRef.innerHTML = markup;
+
+galleryRef.addEventListener('click', clickImageHandler);
+
+function createGalleryItems(items) {
+  return items
+    .map(
+      ({ preview, description, original }) =>
+        `<li class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} alt=${description}/></a></li>`
+    )
+    .join('');
+}
+
+function clickImageHandler(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  lightbox.open();
+}
