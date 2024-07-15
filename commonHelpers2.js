f0b89c364c022d6f8b55fb5bfd1bca70a17ab59e
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2024-07-15 14:42:39 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-07.2@deba23949a5b0cbb5604e24ec26fe70417fc6772 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,6 @@
-import{g as n}from"./assets/gallery-items-d77370d5.js";/* empty css                      */const l=document.querySelector(".gallery"),i=c(n);l.innerHTML=i;l.addEventListener("click",o);function c(e){return e.map(({preview:a,description:r,original:t})=>`<li class="gallery__item"><a class="gallery__link" href=${t}><img class="gallery__image" src=${a} alt=${r}/></a></li>`).join("")}function o(e){if(e.preventDefault(),e.target.nodeName==="IMG"){var a=new SimpleLightbox(".gallery a",{captionsData:"alt",captionDelay:250});a.open()}}
+import{g as n}from"./assets/gallery-items-d77370d5.js";/* empty css                      */const a=document.querySelector(".gallery"),i=c(n);a.innerHTML=i;a.addEventListener("click",m);function c(e){return e.map(({preview:l,description:r,original:t})=>`<li class="gallery__item">
+            <a class="gallery__link" href=${t}>
+                <img class="gallery__image"  src=${l} alt="${r}"/>
+            </a>
+         </li>`).join("")}function m(e){e.preventDefault(),e.target.nodeName==="IMG"&&new SimpleLightbox(".gallery a",{captionsData:"alt",captionDelay:250})}
 //# sourceMappingURL=commonHelpers2.js.map
